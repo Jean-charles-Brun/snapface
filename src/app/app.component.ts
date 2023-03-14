6e8382193587d323b77664e608d1220ef66f88c7
kelerm <kazakaa@gmail.com>
2023-03-14 14:19:49 +0100

    comportement de classe dynamique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit{
         title: 'My Goat',
         description: 'I LUV U',
         createdDate: new Date(),
-        snaps: 6,
+        snaps: 50,
         imageUrl: 'https://i.giphy.com/media/cMso9wDwqSy3e/giphy.webp',
         location: 'la mer'
       },
