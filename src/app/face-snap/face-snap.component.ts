c84e5774716e43e2aff82d107c364b8dbdd864a2
kelerm <kazakaa@gmail.com>
2023-03-14 11:30:19 +0100

    Réagissez aux événements

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,6 +12,7 @@ export class FaceSnapComponent implements OnInit{
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  butonText!:string;
 
   ngOnInit(): void {
     this.title = 'Meowww';
@@ -19,7 +20,17 @@ export class FaceSnapComponent implements OnInit{
     this.createdDate = new Date();
     this.snaps =6;
     this.imageUrl = 'https://f.hellowork.com/blogdumoderateur/2013/02/nyan-cat-gif-1.gif';
+    this.butonText ='On Snap!';
+  }
 
+  onSnap() {
+    if (this.butonText === 'On Snap!'){
+      this.snaps++;
+      this.butonText = 'Oops, un Snap!';
+    } else {
+      this.snaps--;
+      this.butonText = 'On Snap!';
+    }
   }
 
 
