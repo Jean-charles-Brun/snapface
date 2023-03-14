0702eb3efecc75158a9e631bad497f04957fa857
kelerm <kazakaa@gmail.com>
2023-03-14 13:46:04 +0100

    directive ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit{
       title: 'My Goat',
       description: 'I LUV U',
       createdDate: new Date(),
-      snaps: 0,
+      snaps: 6,
       imageUrl: 'https://i.giphy.com/media/cMso9wDwqSy3e/giphy.webp',
       location: 'la mer'
     };
