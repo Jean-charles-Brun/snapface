75bb1b545c0266762dfc40e36fb6ae9b28057a64
kelerm <kazakaa@gmail.com>
2023-03-14 15:35:19 +0100

    Restructurez votre application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -7,40 +6,7 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-
-
-  faceSnaps!: FaceSnap[];
-  faceSnap!: FaceSnap;
-  otherSnap!: FaceSnap;
-  lastSnap!: FaceSnap;
-
   ngOnInit(): void {
-
-    this.faceSnaps = [
-      {
-        title:'Meowww',
-        description:'Space Cat',
-        createdDate: new Date(),
-        snaps: 250,
-        imageUrl: 'https://f.hellowork.com/blogdumoderateur/2013/02/nyan-cat-gif-1.gif',
-        location: 'Tours'
-      },
-      {
-        title: 'My Goat',
-        description: 'I LUV U',
-        createdDate: new Date(),
-        snaps: 50,
-        imageUrl: 'https://i.giphy.com/media/cMso9wDwqSy3e/giphy.webp',
-        location: 'la mer'
-      },
-      {
-        title: 'Dog Life',
-        description: 'AOWWWWwwwwwwwwwwww',
-        createdDate: new Date(),
-        snaps: 0,
-        imageUrl: 'https://i.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.webp'
-      }
-    ];
   }
 
 }
