708403a51b6199430d732d37786ad205b28c0a1c
kelerm <kazakaa@gmail.com>
2023-03-14 11:07:15 +0100

    string interpolation

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent{
+export class FaceSnapComponent implements OnInit{
+
+  title!: string;
+  description!:string;
+  createdDate!: Date;
+  snaps!: number;
+
+  ngOnInit(): void {
+    this.title = 'Pierro';
+    this.description = 'Mon BF since 86';
+    this.createdDate = new Date();
+    this.snaps =6;
+  }
+
 
 }
