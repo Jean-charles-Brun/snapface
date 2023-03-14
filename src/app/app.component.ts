7c5792e500f9dc07252da53e1ec9c34d0bb7a8a6
kelerm <kazakaa@gmail.com>
2023-03-14 13:53:06 +0100

    directive ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,39 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit{
 
+
+  faceSnaps!: FaceSnap[];
   faceSnap!: FaceSnap;
   otherSnap!: FaceSnap;
   lastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.faceSnap = {
-      title:'Meowww',
-      description:'Space Cat',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: 'https://f.hellowork.com/blogdumoderateur/2013/02/nyan-cat-gif-1.gif',
-      location: 'Tours'
-      };
 
-    this.otherSnap = {
-      title: 'My Goat',
-      description: 'I LUV U',
-      createdDate: new Date(),
-      snaps: 6,
-      imageUrl: 'https://i.giphy.com/media/cMso9wDwqSy3e/giphy.webp',
-      location: 'la mer'
-    };
-
-    this.lastSnap = {
-      title: 'Dog Life',
-      description: 'AOWWWWwwwwwwwwwwww',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: 'https://i.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.webp'
-    }
+    this.faceSnaps = [
+      {
+        title:'Meowww',
+        description:'Space Cat',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://f.hellowork.com/blogdumoderateur/2013/02/nyan-cat-gif-1.gif',
+        location: 'Tours'
+      },
+      {
+        title: 'My Goat',
+        description: 'I LUV U',
+        createdDate: new Date(),
+        snaps: 6,
+        imageUrl: 'https://i.giphy.com/media/cMso9wDwqSy3e/giphy.webp',
+        location: 'la mer'
+      },
+      {
+        title: 'Dog Life',
+        description: 'AOWWWWwwwwwwwwwwww',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://i.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.webp'
+      }
+    ];
   }
-
-
 
 }
